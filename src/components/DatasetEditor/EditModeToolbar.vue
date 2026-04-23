5a16593d594f1a2c0a6d942478b158432edc6123
Manuel Garcia <gilbert.mg22@gmail.com>
2026-04-23 16:55:07 +0200

    add EditModeToolbar component with initial template and script setup

---
+++ src/components/DatasetEditor/EditModeToolbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="edit-mode-toolbar">
+       <o-tooltip v-for="mode in visibleModes" :key="mode.key" :label="mode.lable" :position="right">
+        <o-button 
+            :variant="store.editModeKey === mode.key ? 'primary' : 'white'"
+            size="small"
+            :icon-left="mode.icon"
+            icon-pack="fas"
+            @click="store.setEditMode(mode.key)"
+            class="mode-btn"
+        /> 
+        </o-tooltip>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useEditorStore } from '@movici-flow-lib/stores/editor';
+
+// TODO: continue here
+
+</script>
