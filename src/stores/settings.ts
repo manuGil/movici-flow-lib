7a97fa291197586d34a14d2f2d7dc02c513ca4d3
Manuel Garcia <gilbert.mg22@gmail.com>
2026-04-23 16:55:50 +0200

    add useSettingsStore for managing application settings and local storage

---
+++ src/stores/settings.ts
@@ -0,0 +1,63 @@
+import { defineStore } from "pinia";
+import pick from "lodash/pick";
+
+interface ApplicationSettings { 
+    locale: string;
+    baseURL: string;
+    features: string[];
+    projections: Record<string, string>;
+}
+//  TODO: this might not be necessary. Check how settings are managed in the app.
+const defaultSettings: ApplicationSettings = {
+    locale: "en",
+    baseURL: import.meta.env.VITE_MOVICI_BASE_URL || "/",
+    features: [],
+    projections: {},
+};
+const localSetttingsKeys: (keyof ApplicationSettings)[] = ["locale"];
+
+export const useSettingsStore = defineStore("settings", {
+    state: () => ({ ...defaultSettings }),
+    actions: {
+        hasFeature(feature: string) {
+            if (!this.features) {
+                return false;
+            }
+            try {
+                return this.features.includes(feature);
+            } catch {
+                return false;
+            }
+        },
+
+        loadLocal() {
+            let localSettings: Partial<ApplicationSettings>;
+            try {
+                localSettings = pick(JSON.parse(localStorage.settings), ...localSetttingsKeys);
+            } catch {
+                localSettings = {};
+            }
+
+            for (const key in localSettings) {
+                assign(
+                    this,
+                    key as keyof ApplicationSettings,
+                    localSettings[key as keyof ApplicationSettings],
+                );
+            }
+        },
+
+        storeLocal() {
+            localStorage.settings = JSON.stringify(
+                localSetttingsKeys.reduce((curr, key) => {
+                    assign(curr, key, this[key]);
+                    return curr;
+                }, {} as Partial<ApplicationSettings>)
+            );
+        },
+    },
+});
+
+function assign<T, K extends keyof T>(obj: T, key: keyof T, value: unknown) {
+    obj[key] = value as T[K];
+}
