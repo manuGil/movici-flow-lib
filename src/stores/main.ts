c18a5e361a088dd3dfa9760ae37153f23b6671f7
Manuel Garcia <gilbert.mg22@gmail.com>
2026-07-06 06:59:47 +0200

    fix: type import

--- src/stores/main.ts
+++ src/stores/main.ts
@@ -1,12 +1,12 @@
 import { defaultClient } from "../api/client";
-import { IClient } from "../types";
+import type { IClient } from "../types";
 import { global } from "@/i18n";
 import { defineStore } from "pinia";
 import { type Ref, ref } from "vue";
 import { useSettingsStore } from "./settings";
 import { useSnackbar } from "@movici-flow-lib/baseComposables/useSnackbar";
 import { setClient, setProjections } from "@movici-flow-lib/crs";
-// FIXME: implement solution to handle data with current dataset api
+
 export const useMainStore = defineStore("main", () => {
   const client: Ref<IClient> = ref(
     defaultClient({
