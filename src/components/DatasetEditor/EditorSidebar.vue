3c97f816e7f984f18ee984d0072fdd7984a6110a
Manuel Garcia <gilbert.mg22@gmail.com>
2026-09-02 18:00:05 +0200

    clean editorSidebar

--- src/components/DatasetEditor/EditorSidebar.vue
+++ src/components/DatasetEditor/EditorSidebar.vue
@@ -14,52 +14,13 @@
           </option>
         </o-select>
       </o-field>
-      <o-field label="New entity group" label-class="is-size-7" class="mt-2 mb-0">
-        <div class="is-flex is-align-items-center add-row">
-          <o-input
-            v-model="newGroupName"
-            size="small"
-            placeholder="entities"
-            expanded
-            :disabled="!store.dataset"
-            @keyup.enter="onAddEntityGroup"
-          />
-          <o-select v-model="newGroupGeometry" size="small">
-            <option value="point">point</option>
-            <option value="linestring">line</option>
-            <option value="polygon">polygon</option>
-          </o-select>
-          <o-button size="small" :disabled="!canAddGroup" @click="onAddEntityGroup">Add</o-button>
-        </div>
-      </o-field>
-      <p v-if="addGroupError" class="is-size-7 has-text-danger mt-1">{{ addGroupError }}</p>
+
       <div class="is-size-7 has-text-grey mt-1">
         {{ entityCount }} entities
         <span v-if="modifiedCount > 0" class="has-text-warning-dark ml-2">
           ({{ modifiedCount }} modified)
         </span>
       </div>
-      <o-field label="New attribute" label-class="is-size-7" class="mt-2 mb-0">
-        <div class="is-flex is-align-items-center add-row">
-          <o-input
-            v-model="newAttrName"
-            size="small"
-            placeholder="attribute.name"
-            expanded
-            :disabled="!store.entityGroup"
-            @keyup.enter="onAddAttribute"
-          />
-          <o-select v-model="newAttrType" size="small">
-            <option value="number">number</option>
-            <option value="string">string</option>
-            <option value="boolean">boolean</option>
-          </o-select>
-          <o-button size="small" :disabled="!canAddAttribute" @click="onAddAttribute">
-            Add
-          </o-button>
-        </div>
-      </o-field>
-      <p v-if="addAttrError" class="is-size-7 has-text-danger mt-1">{{ addAttrError }}</p>
     </div>
     <div class="sidebar-content p-3">
       <PropertyEditor
@@ -77,7 +38,6 @@
 import { computed, ref } from "vue";
 import { useEditorStore } from "@movici-flow-lib/stores/editor";
 import PropertyEditor from "./PropertyEditor.vue";
-import type { GeometryType } from "@movici-flow-lib/utils/geoJsonBridge";
 
 const store = useEditorStore();
 
@@ -87,40 +47,6 @@ const selectedGroup = computed({
     if (val) store.selectEntityGroup(val);
   },
 });
-
-const newAttrName = ref("");
-const newAttrType = ref<"number" | "string" | "boolean">("number");
-const addAttrError = ref<string | null>(null);
-
-const canAddAttribute = computed(() => !!store.entityGroup && newAttrName.value.trim().length > 0);
-
-function onAddAttribute() {
-  if (!store.entityGroup || !canAddAttribute.value) return;
-  const name = newAttrName.value.trim();
-  if (store.addAttribute(store.entityGroup, name, newAttrType.value)) {
-    newAttrName.value = "";
-    addAttrError.value = null;
-  } else {
-    addAttrError.value = `Cannot add attribute '${name}': name is reserved or already exists`;
-  }
-}
-
-const newGroupName = ref("");
-const newGroupGeometry = ref<GeometryType>("point");
-const addGroupError = ref<string | null>(null);
-
-const canAddGroup = computed(() => !!store.dataset?.data && newGroupName.value.trim().length > 0);
-
-function onAddEntityGroup() {
-  if (!canAddGroup.value) return;
-  const name = newGroupName.value.trim();
-  if (store.addEntityGroup(name, newGroupGeometry.value)) {
-    newGroupName.value = "";
-    addGroupError.value = null;
-  } else {
-    addGroupError.value = `Cannot add entity group '${name}': it already exists`;
-  }
-}
 
 const entityCount = computed(() => {
   if (!store.dataset?.data || !store.entityGroup) return 0;
